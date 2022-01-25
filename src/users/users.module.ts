2b95c7c1e7333ab25f044a8c1d16f05d904374dc
Kristi Achilleos <Krispy2009@gmail.com>
2022-01-25 18:41:20 +0200

    add entities

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], //Creates the repository
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+
 })
-export class UsersModule {}
+export class UsersModule { }
